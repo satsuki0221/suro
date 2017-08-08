7c2fb2a880566822269aa6013d6ec3ddbc87642d
yugo matsumoto <ymmo.9m@gmail.com>
2017-08-09 01:36:21 +0900

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,34 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
+import {observable} from "mobx";
+import {observer} from 'mobx-react';
 
 import { Hello } from './components/Hello';
 
+class AppState {
+  id = Math.random();
+  @observable counter = 0;
+}
+
+interface Props {
+  data: {
+    counter: number;
+  }
+}
+
+@observer
+class App extends React.Component<Props> {
+  render() {
+    return <div className="app">{this.props.data.counter}</div>
+  }
+}
+
+const appState = new AppState();
 ReactDom.render(
-  <Hello compiler="Typescript" framework="React" />,
-  document.getElementById('app'),
+  <App data={appState} />,
+  document.getElementById('app')
 );
+
+setInterval(() => {
+  appState.counter += 1;
+}, 1000);
